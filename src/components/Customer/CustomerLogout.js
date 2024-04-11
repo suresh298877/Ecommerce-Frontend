646474bb50f30c89fa6ca567bb83f6eff445158e
suresh298877 <121373235+suresh298877@users.noreply.github.com>
2024-04-11 22:43:11 +0530

    Update CustomerLogout.js

--- src/components/Customer/CustomerLogout.js
+++ src/components/Customer/CustomerLogout.js
@@ -2,7 +2,7 @@ function CustomerLogout(){
     localStorage.removeItem('customer_login');
     localStorage.removeItem('customer_username');
     localStorage.removeItem('customer_id');
-    window.location.href='/customer/login';
+    window.location.href='/';
 }
 
 export default CustomerLogout;
