26613dd38145dd0ce2f50f5d1d71145832e5f033
suresh298877 <121373235+suresh298877@users.noreply.github.com>
2024-04-11 22:49:47 +0530

    Update SellerLogout.js

--- src/components/Seller/SellerLogout.js
+++ src/components/Seller/SellerLogout.js
@@ -2,7 +2,7 @@ function SellerLogout(){
     localStorage.removeItem('vendor_login');
     localStorage.removeItem('vendor_username');
     localStorage.removeItem('vendor_id');
-    window.location.href='/seller/login';
+    window.location.href='/';
 }
 
 export default SellerLogout;
